4c333298bca85ad130a84a10aaf980f5b8fbc950
JD Olson <dimanche.nav@gmail.com>
2018-09-18 00:49:16 +0000

    added initial components

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
-//include images into your bundle
-import rigoImage from '../../img/rigo-baby.jpg';
+import Header from './_header.jsx';
+import Footer from './_footer.jsx';
 
 //create your first component
 export class Home extends React.Component{
     
     render(){
         return (
-            <div className="text-center mt-5">
-                <h1>Hello Rigo!</h1>
-                <p><img src={rigoImage} /></p>
-                <a href="#" className="btn btn-success">If you see this green button... bootstrap is working</a>
-                <p>Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with love!</p>
+            <div body>
+                <Header />
+                <h1>This will be the Bootstrap Page</h1>
+                <Footer />
             </div>
         );
     }
