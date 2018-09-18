cd2f49c56fdf6d3731116a3452f51b654821f2ed
Cathrine BienAime <cathrinebienaime@yahoo.com>
2018-09-18 01:02:51 +0000

    added jumbotron

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 //include images into your bundle
 import rigoImage from '../../img/rigo-baby.jpg';
 
+import HelloWorld from "./_jumbotron.jsx";
+
 //create your first component
 export class Home extends React.Component{
     
     render(){
         return (
             <div className="text-center mt-5">
-                <h1>Hello Rigo!</h1>
-                <p><img src={rigoImage} /></p>
-                <a href="#" className="btn btn-success">If you see this green button... bootstrap is working</a>
-                <p>Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with love!</p>
+                <HelloWorld />
+               
             </div>
         );
     }
