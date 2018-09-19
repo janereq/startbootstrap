518f89b6f01b7d5b12b658e2cf480083d66866eb
JD Olson <dimanche.nav@gmail.com>
2018-09-19 23:56:19 +0000

    added compatability

--- src/js/component/_cards.jsx
+++ src/js/component/_cards.jsx
@@ -5,25 +5,27 @@ export default class Card extends React.Component{
     
     render(){
         return (
-           <div class= "card-deck"> 
-           <Cards /> 
-           <Cards /> 
-           <Cards /> 
-           <Cards />
-           </div>
+            <div className= "card-deck"> 
+                <Cards /> 
+                <Cards /> 
+                <Cards /> 
+                <Cards />
+            </div>
         );
     }
 }
 
 function Cards(){
-  return(<div class="card">
-                <img src="https://via.placeholder.com/286x180" alt="Card image cap" />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                </div>
-                 <div className="card-footer text-center">
-                     <a href="#" class="btn btn-primary">Find out more!!</a>
-                 </div>
-            </div>);
-            }
+    return(
+        <div className="card">
+            <img src="https://via.placeholder.com/286x180" alt="Card image cap" />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div className="card-footer text-center">
+                <a href="#" className="btn btn-primary">Find out more!!</a>
+            </div>
+        </div>
+    );
+}
