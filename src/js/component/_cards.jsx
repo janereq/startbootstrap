852218015127035e40591790c66c696004a063ca
Janere Quijano <janereq@gmail.com>
2018-09-19 23:34:19 +0000

    jayyyy

--- src/js/component/_cards.jsx
+++ src/js/component/_cards.jsx
@@ -5,16 +5,25 @@ export default class Card extends React.Component{
     
     render(){
         return (
-            <div className="card-deck">
-                <div className="card">
-                    <img className="card-img-top" src="..." alt="Card image cap"/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-            </div>
+           <div class= "card-deck"> 
+           <Cards /> 
+           <Cards /> 
+           <Cards /> 
+           <Cards />
+           </div>
         );
     }
 }
+
+function Cards(){
+  return(<div class="card">
+                <img src="https://via.placeholder.com/286x180" alt="Card image cap" />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                 <div className="card-footer text-center">
+                     <a href="#" class="btn btn-primary">Find out more!!</a>
+                 </div>
+            </div>);
+            }
